954e8f7fe90f2dfdf1339f0bec95f064e5b69184
Hakan Kaya <80759003+Alcano22@users.noreply.github.com>
2024-12-25 21:16:13 +0100

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,34 @@
+project "Enet"
+    kind "StaticLib"
+    language "C"
+    staticruntime "off"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "include/**.h",
+        "src/**.c"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+            "ENET_STATIC"
+        }
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
